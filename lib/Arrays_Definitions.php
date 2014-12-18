3aa880e0fbb4632301134361e65a72cb8c8397a1
kjvtough <kjvtough@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-18 23:48:09 +0000

    Tagging for release 2.3
    git-svn-id: https://plugins.svn.wordpress.org/timed-content/tags/2.3@1048675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/Arrays_Definitions.php
+++ lib/Arrays_Definitions.php
@@ -7,28 +7,28 @@ if ( !isset( $timed_content_rule_freq_array ) )
         4 => __( "Yearly", 'timed-content' ) );
 
 if ( !isset( $timed_content_rule_days_array ) )
-	$timed_content_rule_days_array = array( 0 => __( "Sundays", 'timed-content' ),
-						1 => __( "Mondays", 'timed-content' ), 
-						2 => __( "Tuesdays", 'timed-content' ), 
-						3 => __( "Wednesdays", 'timed-content' ), 
-						4 => __( "Thursdays", 'timed-content' ), 
-						5 => __( "Fridays", 'timed-content' ), 
-						6 => __( "Saturdays", 'timed-content' ) );
+    $timed_content_rule_days_array = array( 0 => __( "Sundays", 'timed-content' ),
+        1 => __( "Mondays", 'timed-content' ),
+        2 => __( "Tuesdays", 'timed-content' ),
+        3 => __( "Wednesdays", 'timed-content' ),
+        4 => __( "Thursdays", 'timed-content' ),
+        5 => __( "Fridays", 'timed-content' ),
+        6 => __( "Saturdays", 'timed-content' ) );
 
 if ( !isset( $timed_content_rule_ordinal_array ) )
-	$timed_content_rule_ordinal_array = array( 0 => __( "first", 'timed-content' ),
-							1 => __( "second", 'timed-content' ),
-							2 => __( "third", 'timed-content' ),
-							3 => __( "fourth", 'timed-content' ),
-							4 => __( "last", 'timed-content' ) );
+    $timed_content_rule_ordinal_array = array( 0 => __( "first", 'timed-content' ),
+        1 => __( "second", 'timed-content' ),
+        2 => __( "third", 'timed-content' ),
+        3 => __( "fourth", 'timed-content' ),
+        4 => __( "last", 'timed-content' ) );
 
 if ( !isset( $timed_content_rule_ordinal_days_array ) )
-	$timed_content_rule_ordinal_days_array = array( 0 => __( "Sunday", 'timed-content' ),
-						1 => __( "Monday", 'timed-content' ), 
-						2 => __( "Tuesday", 'timed-content' ), 
-						3 => __( "Wednesday", 'timed-content' ), 
-						4 => __( "Thursday", 'timed-content' ), 
-						5 => __( "Friday", 'timed-content' ), 
-						6 => __( "Saturday", 'timed-content' ),
-						7 => __( "day", 'timed-content' ) );
+    $timed_content_rule_ordinal_days_array = array( 0 => __( "Sunday", 'timed-content' ),
+        1 => __( "Monday", 'timed-content' ),
+        2 => __( "Tuesday", 'timed-content' ),
+        3 => __( "Wednesday", 'timed-content' ),
+        4 => __( "Thursday", 'timed-content' ),
+        5 => __( "Friday", 'timed-content' ),
+        6 => __( "Saturday", 'timed-content' ),
+        7 => __( "day", 'timed-content' ) );
 ?>
